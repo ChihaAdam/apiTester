3a0896597e8e3d151eb8c3d6d38f35f077163b2d
adam <chihaadam137@gmail.com>
2025-07-16 17:45:31 +0100

    simple fixes

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import {requestHandler} from "../lib/requestHandler"; 
 import type { headersType } from "../context/headersStore";
 
+function formed(url: string): string {
+    let trimmedUrl = url.trim();
+    trimmedUrl = trimmedUrl.startsWith("http") ? trimmedUrl : `http://${trimmedUrl}`;
+    return trimmedUrl.endsWith("/") ? trimmedUrl.slice(0, -1) : trimmedUrl;
+}
+
+
+
 export default function useFetch(url: string, method: string,headers: headersType, body?: string) {
     const [response,setResponse] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
+    const formedUrl = formed(url);
     async function fetchData() {
         setLoading(true);
         try {
-            const response = await requestHandler(url, method,headers, body);
+            const response = await requestHandler(formedUrl, method,headers, body);
             setResponse(response);
         } catch (error) {
             setResponse("");
